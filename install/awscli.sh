613fce34f696579364bed13db57a2037c9c30ae3
Nick Antonelli <nick.antonelli@shadowhealth.com>
2018-02-28 13:39:39 -0500

    exit with nonzero code on an error

--- install/awscli.sh
+++ install/awscli.sh
@@ -14,16 +14,19 @@ curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "$AWS_DIR/$ZIP"
 
 if [ ! -f "$AWS_DIR/$ZIP" ]; then
     echo "ERROR unable to download $ZIP"
+    exit 1;
 fi
 
 unzip "$AWS_DIR/$ZIP" -d "$AWS_DIR"
 
 if [ ! -d "$AWS_DIR/$ZIP_DIR" ]; then
     echo "ERROR unable to unzip $ZIP"
+    exit 1;
 fi
 
 sudo "$AWS_DIR/$ZIP_DIR/install" -i $PATH_INSTALL -b $PATH_BIN
 
 if [ "$?" -eq "0" ]; then
     rm -rf "$AWS_DIR/$ZIP" "$AWS_DIR/$ZIP_DIR" "$AWS_DIR/cli"
+    exit 1;
 fi
